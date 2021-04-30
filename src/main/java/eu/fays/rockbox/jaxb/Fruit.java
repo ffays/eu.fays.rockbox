c185b6f929749da42a647f55c7f4086913d10067
Frederic Fays <frederic@fays.eu>
2021-04-30 09:38:34 +0200

    Moved JAXB essays from the sandbox to the rockbox

---
+++ src/main/java/eu/fays/rockbox/jaxb/Fruit.java
@@ -0,0 +1,23 @@
+package eu.fays.rockbox.jaxb;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+/**
+ * An enum to play with.
+ * @author Fr&eacute;d&eacute;ric Fays
+ */
+public enum Fruit {
+	ORANGE, BANANA, APPLE, MANGO, APRICOT;
+
+	/** Default value */
+	public static final Fruit DEFAULT = APPLE;
+	
+	/**
+	 * Returns all enumeration elements as a new stream
+	 * @return the new stream
+	 */
+	public static Stream<Fruit> fruitStream() {
+		return Arrays.stream(values());
+	}
+}
---
+++ src/main/java/eu/fays/rockbox/jaxb/IntegerXmlAdapter.java
@@ -0,0 +1,9 @@
+package eu.fays.rockbox.jaxb;
+
+public class IntegerXmlAdapter extends GenericXmlAdapter<Integer> {
+
+	public IntegerXmlAdapter() {
+		super(0);
+	}
+
+}
