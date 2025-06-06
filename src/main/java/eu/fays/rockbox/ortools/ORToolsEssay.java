59af3769f8e82354d5ceac274a6ef1946c8dbf16
Frédéric Fays <frederic@fays.eu>
2025-06-06 11:51:55 +0200

    OR-Tools windows ...

--- src/main/java/eu/fays/rockbox/ortools/ORToolsEssay.java
+++ src/main/java/eu/fays/rockbox/ortools/ORToolsEssay.java
@@ -1,5 +1,6 @@
 package eu.fays.rockbox.ortools;
 
+import java.io.File;
 import java.util.logging.Logger;
 
 import com.google.ortools.constraintsolver.ConstraintSolverParameters;
@@ -10,7 +11,7 @@ import com.google.ortools.init.OrToolsVersion;
 
 // Zsh
 /*
-mvn clean package
+mvn clean dependency:copy-dependencies package
 find ~/.m2/repository/com/google/ortools -type f -name '*64-9.12.4544.jar' -exec sh -c 'cd $(dirname $1) && jar xvf $1' _ {} \;
 find ~/.m2/repository/com/google/ortools -type f -name '*jniortools*' -exec dirname {} \; | cut -c $((${#HOME}+2))- | sed 's|^|${system_property:user.home}/|' | paste -s -d ":" -
  */
@@ -27,6 +28,14 @@ find ~/.m2/repository/com/google/ortools -type f -name '*jniortools*' -exec dirn
 @SuppressWarnings("nls")
 public class ORToolsEssay {
 
+	/** Java command */
+	public static final String JAVA_COMMAND = """
+java -cp "target/eu.fays.rockbox-1.0.0.jar:$(ls -1 target/dependency/*.jar | paste -s -d ':' -)" \
+  -Djava.util.logging.SimpleFormatter.format='%5$s%6$s%n' \
+  -Djava.library.path="$(dirname ~/.m2/repository/com/google/ortools/**/*jniortools* | paste -s -d ':' -)" \
+  eu.fays.rockbox.ortools.ORToolsEssay
+""";
+	
 	/** Standard logger */
 	private static final Logger LOGGER = Logger.getLogger(ORToolsEssay.class.getName());
 
@@ -41,6 +50,18 @@ public class ORToolsEssay {
 	 * @param args unused
 	 */
 	public static void main(String[] args) {
+		if (System.getProperty("os.name").indexOf("Windows") != -1) {
+			final File javaLibraryPath = new File(System.getProperty("java.library.path"));
+			System.load(new File(javaLibraryPath, "zlib1.dll").getAbsolutePath());
+			System.load(new File(javaLibraryPath, "utf8_validity.dll").getAbsolutePath());
+			System.load(new File(javaLibraryPath, "highs.dll").getAbsolutePath());
+			System.load(new File(javaLibraryPath, "abseil_dll.dll").getAbsolutePath());
+			System.load(new File(javaLibraryPath, "re2.dll").getAbsolutePath());
+			System.load(new File(javaLibraryPath, "libprotobuf.dll").getAbsolutePath());
+			System.load(new File(javaLibraryPath, "ortools.dll").getAbsolutePath());
+			System.load(new File(javaLibraryPath, "jniortools.dll").getAbsolutePath());
+		}
+		
 		System.loadLibrary("jniortools");
 		LOGGER.info("OR-Tools: " + OrToolsVersion.getVersionString());
 		LOGGER.info("Foreword: We are seing 20 heads and 56 legs.");
